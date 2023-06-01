f0bd103fc511958a4b9f49878ac6da0869c11564
Tcharlles <tcharlles3@gmail.com>
2023-06-01 00:03:28 -0300

    fix-end

--- gsap.js
+++ gsap.js
@@ -61,7 +61,7 @@ gsap.to(galeria1, {
     trigger: ".projetos",
     start: "top top",
     // end: () => "+=" + projetosGaleria.offsetWidth,
-    end: "+=4000",
+    end: "+=2000",
     pin: true,
     scrub: 1,
     snap: 1 / (galeria1.length - 1),
