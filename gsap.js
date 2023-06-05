c6a8407e42ca3baca8cafed06259ca4a68eed699
Tcharlles <tcharlles3@gmail.com>
2023-06-05 17:33:41 -0300

    fix-gsap

--- gsap.js
+++ gsap.js
@@ -60,21 +60,21 @@ gsap.to(galeria1, {
   scrollTrigger: {
     trigger: ".projetos",
     start: "top top",
-    end: () => "+=" + projetosGaleria.offsetWidth,
-    //end: "+=700",
+    // end: () => "+=" + projetosGaleria.offsetWidth,
+    end: "+=700",
     pin: true,
-    scrub: 0.2,
+    scrub: 1,
     snap: 1 / (galeria1.length - 1),
   },
 });
 
-let mm = gsap.matchMedia();
-mm.add("(max-width: 450px)", () => {
-  gsap.to(galeria1, {
-    end: "+=700",
-    scrub: 1,
-  });
-});
+// let mm = gsap.matchMedia();
+// mm.add("(max-width: 450px)", () => {
+//   gsap.to(galeria1, {
+//     end: "+=700",
+//     scrub: 1,
+//   });
+// });
 
 /// PIN VERTICAL SCROLL
 
